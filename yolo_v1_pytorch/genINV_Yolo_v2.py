Fixed fc layers, removed sigmoid dependancy, and adjust loss function w,h calculation

yolo_v1_pytorch/genINV_Yolo_v2.py
@@ -101,7 +101,7 @@ class ImageNetVID(data.Dataset):
 
         self.data_set = pd.read_pickle(path_to_dataset)
         self.unique_keys = self.getKeys(pd.DataFrame(self.data_set))
-        #self.data_set = self.data_set[:10]
+        #self.data_set = self.data_set[:1000]
 
         self.network_dim = image_size
         mean_rgb = [122.67891434, 116.66876762, 104.00698793]
@@ -304,7 +304,7 @@ class ImageNetVID(data.Dataset):
         h_sample, w_sample, _ = sample_img.shape
  
         #Normalize Bounding boxes
-
+        #test_samplebox = sample_bbox
         sample_bbox = sample_bbox/((torch.tensor([w_sample,h_sample,w_sample,h_sample],dtype=torch.float)).view(1,-1))
 
         #Encoding Target
@@ -337,4 +337,5 @@ class ImageNetVID(data.Dataset):
         #return sample_img,sample_bbox
 
         return images,target
+        #return images,test_samplebox,target
             

yolo_v1_pytorch/loss.py
@@ -150,7 +150,15 @@ class Loss(nn.Module):
         bbox_target_response = bbox_target[coord_response_mask].view(-1, 5)  # [n_response, 5], only the first 4=(x, y, w, h) are used
         target_iou = bbox_target_iou[coord_response_mask].view(-1, 5)        # [n_response, 5], only the last 1=(conf,) is used
         loss_xy = F.mse_loss(bbox_pred_response[:, :2], bbox_target_response[:, :2], reduction='sum')
-        loss_wh = F.mse_loss(torch.sqrt(bbox_pred_response[:, 2:4]), torch.sqrt(bbox_target_response[:, 2:4]), reduction='sum')
+
+        #Handling negative square root loss
+        loss_wh = F.mse_loss(
+            torch.sign(bbox_target_response[:, 2:4])*torch.sqrt(torch.abs(bbox_target_response[:, 2:4])+1e-8),
+            torch.sign(bbox_pred_response[:, 2:4])*torch.sqrt(torch.abs(bbox_pred_response[:, 2:4])+1e-8),
+            reduction='sum'
+        )
+
+        ##loss_wh = F.mse_loss(torch.sqrt(bbox_pred_response[:, 2:4]), torch.sqrt(bbox_target_response[:, 2:4]), reduction='sum')
         loss_obj = F.mse_loss(bbox_pred_response[:, 4], target_iou[:, 4], reduction='sum')
 
         # Class probability loss for the cells which contain objects.

yolo_v1_pytorch/r50_yolo.py
@@ -149,6 +149,7 @@ class ResNet(nn.Module):
                                bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
+        #self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
@@ -160,11 +161,20 @@ class ResNet(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         #Custom for locem_multihead
         #num_classes = 7*7*(2*5+30+64)
-        num_classes = self.S*self.S*(self.X*self.B + self.C)
-        #self.l1 = nn.Linear(512 * block.expansion,4096)
-        #self.relu2 = nn.ReLU()
-        self.fc_locem = nn.Linear(2048, num_classes)
-        self.sigmoid = nn.Sigmoid()
+        yolo_out = self.S*self.S*(self.X*self.B + self.C)
+        ##self.l1 = nn.Linear(512 * block.expansion,4096)
+        #self.relu2 = nn.ReLU(inplace=True)
+        ##self.fc_yolo = nn.Linear(4096, yolo_out,bias=False)
+        #self.checkLayer = nn.Linear(4096,yolo_out,bias=False)
+        #self.sigmoid = nn.Sigmoid()
+
+        #New Sequential
+        self.yolob = nn.Sequential(
+            nn.Linear(2048,4096),
+            nn.Dropout(),
+            nn.LeakyReLU(0.1),
+            nn.Linear(4096, yolo_out)
+        )
         
         
 
@@ -225,14 +235,17 @@ class ResNet(nn.Module):
 
         #y=torch.flatten(x,1)
         y=torch.flatten(x,1)
-
+        # x = x.view(x.size(0), -1)
         x = torch.flatten(x, 1)
-        #x = self.l1(x)
+        ##x = F.relu(self.l1(x))
         #x = self.relu2(x)
-        x = self.fc_locem(x)
+        ##x = self.fc_yolo(x)
+       
+        x = self.yolob(x)
+
         x = x.view(-1,self.S,self.S,self.X*self.B+self.C)
         #x[:,:,:,:self.X*self.B+self.C]=self.sigmoid(x[:,:,:,:self.X*self.B+self.C])
-        x = self.sigmoid(x)
+        ##x = self.sigmoid(x)
         #x[:,:,:,40:]=F.normalize(x[:,:,:,40:],p=2)
 
         return x,y

yolo_v1_pytorch/train_yolo.py
@@ -23,6 +23,10 @@ import torchvision.models as models
 sys.path.append('..')
 from genINV_Yolo_v2 import ImageNetVID
 
+from pytorch_model_summary import summary
+
+torch.autograd.set_detect_anomaly(True)
+
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
@@ -44,7 +48,8 @@ print('CUDA device_count: {}'.format(torch.cuda.device_count()))
 #checkpoint_path = 'weights/darknet/model_best.pth.tar'
 
 path_to_disk = '/mnt/data1/shravank/results/locem/main/run/'
-experiment_name = 'g4_yolob_e300b64_v1.1'              #'g4_yolob_e300b64_v1'
+experiment_name = 'g5_yolob_e300b64_v1.03'
+#experiment_name = 'g5_yolob_e300b192_v1'              #'g4_yolob_e300b64_v1'
 path_to_disk = path_to_disk + experiment_name + '/'
 # Frequency to print/log the results.
 print_freq = 5
@@ -52,26 +57,22 @@ tb_log_freq = 5
 
 #Network Parameteres
 S=7
-B=5
+B=2
 X=5
 C=30
 image_size = 448
 
 # Training hyper parameters.
 init_lr = 0.001
-base_lr = 0.01
+base_lr = 0.001
 momentum = 0.9
-weight_decay = 1.0e-4
+weight_decay = 5e-4
 num_epochs = 300
 batch_size = 64
 
-def class_decoder(self,pred_tensor,target_tensor,accuracy):
+'''def class_decoder(self,pred_tensor,target_tensor,accuracy):
 
-    '''
-        Args:
-        Out:
-    '''
-
+   
     S,B,C,X,gamma = self.S,self.B,self.C,self.X,self.gamma
 
     #Extract the mask of targets with a gamma value 1,2,3. This will give us a location as to where the boxes are and their class embedding respectively
@@ -95,18 +96,20 @@ def class_decoder(self,pred_tensor,target_tensor,accuracy):
 
     acc1, acc5 = accuracy(output, target, topk=(1, 5))
 
-    return acc1, acc5
+    return acc1, acc5'''
 
 # Learning rate scheduling.
 def update_lr(optimizer, epoch, burnin_base, burnin_exp=4.0):
     if epoch == 0:
         lr = init_lr + (base_lr - init_lr) * math.pow(burnin_base, burnin_exp)
     elif epoch == 1:
-        lr = base_lr
-    elif epoch == 75:
         lr = 0.001
-    elif epoch == 105:
-        lr = 0.0001
+    elif epoch == 15:
+        lr = 0.001
+    elif epoch == 50: #75
+        lr = 0.0001 #0.001
+    elif epoch == 80: #105
+        lr = 0.00001 #0.0001
     else:
         return
 
@@ -146,6 +149,18 @@ else:
 
 #print(yolo)
 
+
+
+# show input shape
+#print(summary(yolo, torch.rand((10, 3, 448, 448)), show_input=True))
+
+# show output shape
+#print(summary(yolo, torch.rand((10, 3, 448, 448)), show_input=False))
+
+# show output shape and hierarchical view of net
+#print(summary(yolo, torch.rand((10, 3, 448, 448)), show_input=False, show_hierarchical=True))
+
+
 #Enable Parallel
 yolo = torch.nn.DataParallel(yolo).cuda()
 
@@ -178,6 +193,8 @@ writer = SummaryWriter(log_dir=path_to_disk)
 logfile = open(os.path.join(log_dir, 'log.txt'), 'w')
 best_val_loss = np.inf
 
+f = open(os.path.join(log_dir,'best_loss_epoch.txt'),'a')
+
 for epoch in range(num_epochs):
     print('\n')
     print('Starting epoch {} / {}'.format(epoch, num_epochs))
@@ -199,8 +216,12 @@ for epoch in range(num_epochs):
         targets = Variable(targets)
         imgs, targets = imgs.cuda(), targets.cuda()
 
+        #print('imgs size'+str(imgs.size()))
+        #print(imgs)
+
         # Forward to compute loss.
-        preds, _ = yolo(imgs)      
+        preds, _ = yolo(imgs)
+        #print(preds)      
         loss, loss_class, loss_box = criterion(preds, targets)
         loss_this_iter = loss.item()
         total_loss += loss_this_iter * batch_size_this_iter
@@ -252,9 +273,15 @@ for epoch in range(num_epochs):
     # Save results.
     logfile.writelines(str(epoch + 1) + '\t' + str(val_loss) + '\n')
     logfile.flush()
+
+    if epoch<102 and epoch%10==0:
+        torch.save(yolo.state_dict(), os.path.join(log_dir, 'model_ep'+str(epoch)+'.pth'))
+
     torch.save(yolo.state_dict(), os.path.join(log_dir, 'model_latest.pth'))
     if best_val_loss > val_loss:
         best_val_loss = val_loss
+        
+        f.write('Epoch: '+str(epoch)+' '+'Loss: '+str(best_val_loss)+'\n')
         torch.save(yolo.state_dict(), os.path.join(log_dir, 'model_best.pth'))
 
     # Print.
@@ -269,5 +296,7 @@ for epoch in range(num_epochs):
     ''' # TensorBoard.
     writer.add_scalar('test/loss', val_loss, epoch + 1)'''
 
+f.close()
+writer.flush()
 writer.close()
 logfile.close()